90abde03e0c8c752404ff10e13591e5b3b889b85
Alex Nichol <alex@Alexs-MacBook-Pro.local>
2012-07-31 17:17:54 -0400

    Initial Commit

---
+++ Lingua2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Lingua2
+//
+//  Created by Alex Nichol on 7/31/12.
+//  Copyright (c) 2012 Alex Nichol. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
